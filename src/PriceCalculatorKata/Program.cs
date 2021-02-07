5711d2bbd28b7fa58686398dca773ac69fa8b861
Tamer <tamer.assaliya@gmail.com>
2021-02-07 20:31:29 +0200

    separate Product model class

--- src/PriceCalculatorKata/Program.cs
+++ src/PriceCalculatorKata/Program.cs
@@ -7,24 +7,24 @@ namespace PriceCalculatorKata
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Product product = new Product()
-            {
-                Name = "The Little Prince",
-                UPC = 12345,
-                Price = 20.25,
-                UPCDiscountPrecedence = DiscountPrecedence.AfterTax,
-                UniversalDiscountPrecedence = DiscountPrecedence.AfterTax,
-                TaxPercentage = 21,
-                UniversalDiscountPercentage = 15,
-                ProductDiscountType = DiscountType.Multiplicative,
-                RegionInfo = new RegionInfo("US"), //use region info like: US, GB, JP 
+            // Product product = new Product()
+            // {
+            //     Name = "The Little Prince",
+            //     UPC = 12345,
+            //     Price = 20.25,
+            //     UPCDiscountPrecedence = DiscountPrecedence.AfterTax,
+            //     UniversalDiscountPrecedence = DiscountPrecedence.AfterTax,
+            //     TaxPercentage = 21,
+            //     UniversalDiscountPercentage = 15,
+            //     ProductDiscountType = DiscountType.Multiplicative,
+            //     RegionInfo = new RegionInfo("US"), //use region info like: US, GB, JP 
 
 
-            };
-            product.AssignAdditionalCost(ValueComputationType.PriceRelative, "Transport", 3);
+            // };
+            // product.AssignAdditionalCost(ValueComputationType.PriceRelative, "Transport", 3);
 
-            product.ReportProductPrice();
-            Console.WriteLine("---");
+            // product.ReportProductPrice();
+            // Console.WriteLine("---");
         }
     }
 }
