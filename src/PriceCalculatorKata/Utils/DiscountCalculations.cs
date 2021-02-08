4956fcd55b6cad1f3175a39ab75ab20365875d28
Tamer <tamer.assaliya@gmail.com>
2021-02-08 18:19:14 +0200

    Cast univarsalDiscountPercentage

--- src/PriceCalculatorKata/Utils/DiscountCalculations.cs
+++ src/PriceCalculatorKata/Utils/DiscountCalculations.cs
@@ -7,7 +7,12 @@ namespace PriceCalculatorKata
         private double Cap = double.MaxValue;
         public Product Product { get; set; }
         public DiscountType ProductDiscountType { get; set; } = DiscountType.Additive;
-        public double UniversalDiscountPercentage { get; set; } = 0.0;
+        private double _universalDiscountPercentage { get; set; } = 0.0;
+        public double UniversalDiscountPercentage
+        {
+            get { return _universalDiscountPercentage; }
+            set { _universalDiscountPercentage = value.CastPercentage(); }
+        }
         public double GetUniversalDiscountAmount()
         {
             double univarsalDiscountAmount = Product.Price * UniversalDiscountPercentage;
@@ -18,7 +23,8 @@ namespace PriceCalculatorKata
             double UPCDiscountAmount = 0;
             if (ProductDiscountType == DiscountType.Additive)
                 UPCDiscountAmount = Product.UPCDiscountPercentage * Product.Price;
-            else if (ProductDiscountType == DiscountType.Multiplicative) UPCDiscountAmount = Product.UPCDiscountPercentage * (Product.Price - GetUniversalDiscountAmount());
+            else if (ProductDiscountType == DiscountType.Multiplicative)
+                UPCDiscountAmount = Product.UPCDiscountPercentage * (Product.Price - GetUniversalDiscountAmount());
             return Math.Round(UPCDiscountAmount, 4);
         }
         public void AssignCapAmount(ValueComputationType valueComputationType, double value)
