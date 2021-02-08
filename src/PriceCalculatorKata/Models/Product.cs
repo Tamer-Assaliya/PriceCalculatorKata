9182624066220f5e8ad9c250e36b6316e62ebaf6
Tamer <tamer.assaliya@gmail.com>
2021-02-08 09:38:04 +0200

    Apply CastPercentage() to UPCDiscountPercentage

--- src/PriceCalculatorKata/Models/Product.cs
+++ src/PriceCalculatorKata/Models/Product.cs
@@ -6,7 +6,12 @@ namespace PriceCalculatorKata
     {
         public string Name { get; set; }
         public int UPC { get; set; }
-        public double UPCDiscountPercentage { get; set; }
         public double Price { get; set; }
+        private double _UPCDiscountPercentage;
+        public double UPCDiscountPercentage
+        {
+            get { return _UPCDiscountPercentage; }
+            set { _UPCDiscountPercentage = value.CastPercentage(); }
+        }
     }
 }
