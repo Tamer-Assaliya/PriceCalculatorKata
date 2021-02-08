4d169e6d31efc7a2ac4897be15c89fcd4f21355d
Tamer <tamer.assaliya@gmail.com>
2021-02-08 09:24:02 +0200

    Implement ProductReport class in ProductReport.cs

---
+++ src/PriceCalculatorKata/Utils/ProductReport.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Globalization;
+
+namespace PriceCalculatorKata
+{
+    class ProductReport
+    {
+        public RegionInfo RegionInfo { private get; set; }
+        public void ReportCostByName(string name, double cost)
+        {
+            double value = Math.Round(cost, 2);
+            string output = $"{name} = {RegionInfo.CurrencySymbol}{value} {RegionInfo.ISOCurrencySymbol}";
+            Console.WriteLine(output);
+        }
+    }
+}
