522037ffeea1289388e0f01f78949e715941ecad
Tamer <tamer.assaliya@gmail.com>
2021-02-10 17:51:28 +0200

    change a private field name to camelCase

--- src/PriceCalculatorKata/Models/Product.cs
+++ src/PriceCalculatorKata/Models/Product.cs
@@ -7,11 +7,11 @@ namespace PriceCalculatorKata
         public string Name { get; set; }
         public int UPC { get; set; }
         public double Price { get; set; }
-        private double _UPCDiscountPercentage;
+        private double _upcDiscountPercentage;
         public double UPCDiscountPercentage
         {
-            get { return _UPCDiscountPercentage; }
-            set { _UPCDiscountPercentage = value.CastPercentage(); }
+            get { return _upcDiscountPercentage; }
+            set { _upcDiscountPercentage = value.CastPercentage(); }
         }
     }
 }
